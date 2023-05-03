79b5e01231de9b058717286f12b236055f5beb98
megaurso <bernadogui@hotmail.com>
2023-05-03 16:26:40 -0300

    feat: finish all models and relations

---
+++ books/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from users.models import User
+
+
+class Followings(models.Model):
+    book_id = models.ForeignKey("books.Book", on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"<following {self.id} = {self.books}]>"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=120)
+    sinopse = models.CharField(max_length=120)
+    year = models.DateTimeField(auto_now_add=True)
+    user = models.ManyToManyField(
+        User, through=Followings, related_name="books"
+    )
+
+    def __str__(self) -> str:
+        return f"<Book {self.id} = {self.title}]>"
---
+++ users/views.py
@@ -0,0 +1,4 @@
+from rest_framework import generics
+
+class UsersView(generics.ListCreateAPIView):
+    ...
