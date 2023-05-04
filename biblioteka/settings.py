ce7933e6d8da8442dff3c2decf1c73dbd1a000bd
megaurso <bernadogui@hotmail.com>
2023-05-04 18:52:53 -0300

    feat create crud books

--- biblioteka/settings.py
+++ biblioteka/settings.py
@@ -44,13 +44,9 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["rest_framework"]
+THIRD_PARTY_APPS = ["rest_framework", "drf_spectacular"]
 
-MY_APPS = [
-    "users",
-    "books",
-    "copies"
-]
+MY_APPS = ["users", "books", "copies"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
@@ -153,4 +149,5 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
