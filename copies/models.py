0cdc40497c83beabc7db14d96e3152953e18f93c
megaurso <bernadogui@hotmail.com>
2023-05-04 14:58:03 -0300

    fix: migration book_loans

--- copies/models.py
+++ copies/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Book_loans(models.Model):
+    STATUS_CHOICES = [
+        ("Not Returned","Not Returned"),
+        ("Returned", "Returned")
+    ]
+
     started_at = models.DateTimeField(auto_now_add=True)
-    finished_at = models.DateTimeField()
-    status = models.TextField()
+    finished_at = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(
+        max_length=15,choices=STATUS_CHOICES,default="Not Returned"
+    )
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     copie = models.ForeignKey("copies.Copies", on_delete=models.CASCADE)
+
+    def edit_finished_at(self):
+        self.finished_at = timezone.now()
+        self.save()
 
     def __str__(self) -> str:
         return f"<following {self.id} = {self.user_id}]>"
