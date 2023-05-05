cfc1a4fdcef4a28d0a0fec67ad04512ffd6801a2
megaurso <bernadogui@hotmail.com>
2023-05-05 15:43:50 -0300

    feat:finish block user

--- copies/views.py
+++ copies/views.py
@@ -2,15 +2,16 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.shortcuts import render
 from rest_framework import generics
 from django.shortcuts import get_object_or_404
-from rest_framework.exceptions import ValidationError 
+from rest_framework.exceptions import ValidationError
 from books.models import Book
 from .serializers import CopiesSerializer
-from .models import Copy
+from .models import Copy, Book_loans
 from .permissions import IsAdm
 from users.serializers import BookLoanSerializer
 from .models import Book_loans
 from users.models import User
 from datetime import datetime
+
 
 # Create your views here.
 
@@ -35,6 +36,7 @@ class CopiesView(RetrieveUpdateDestroyCreateAPIView):
         book_id = get_object_or_404(Book, pk=self.kwargs["pk"])
         serializer.save(book=book_id)
 
+
 class BookLoanView(RetrieveUpdateDestroyCreateAPIView):
     serializer_class = BookLoanSerializer
     queryset = Book_loans
@@ -51,6 +53,17 @@ class BookLoanView(RetrieveUpdateDestroyCreateAPIView):
         copy.is_available = False
         copy.last_loan = datetime.now()
         copy.save()
-        
+
         return serializer.save(user=user_id)
 
+    def perform_update(self, serializer):
+        loan = get_object_or_404(Book_loans, pk=self.kwargs["pk"])
+        user = User.objects.get(pk=loan.user.id)
+
+        day_time = datetime.now().timestamp()
+        finish_date = loan.finished_at.timestamp()
+        result = day_time - finish_date
+        if result > 0:
+            user.is_blocked = True
+            user.save()
+        
